8574c25a7b40f60b4bf68483e8acb73d827f1809
staFF6773 <108166164+staFF6773@users.noreply.github.com>
2024-06-30 21:30:20 -0600

    Create Hello-trash-world#1.rb

---
+++ rubylang/Hello-trash-world#1.rb
@@ -0,0 +1,51 @@
+def h
+  'H'
+end
+
+def e
+  'e'
+end
+
+def l
+  'l'
+end
+
+def o
+  'o'
+end
+
+def space
+  ' '
+end
+
+def w
+  'W'
+end
+
+def r
+  'r'
+end
+
+def d
+  'd'
+end
+
+def exclamation
+  '!'
+end
+
+def newline
+  "\n"
+end
+
+def hello
+  h + e + l + l + o + space
+end
+
+def world
+  w + o + r + l + d + exclamation + newline
+end
+
+output = hello + world
+
+puts output
