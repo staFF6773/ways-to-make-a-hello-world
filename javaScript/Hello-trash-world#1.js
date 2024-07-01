1f4d3fe682ad65cd660d264bf3ec4b1a40bf63dd
staFF6773 <108166164+staFF6773@users.noreply.github.com>
2024-06-30 21:25:05 -0600

    Create Hello-trash-world#1.js

---
+++ javaScript/Hello-trash-world#1.js
@@ -0,0 +1,45 @@
+function h() {
+    return 'H';
+}
+
+function e() {
+    return 'e';
+}
+
+function l() {
+    return 'l';
+}
+
+function o() {
+    return 'o';
+}
+
+function space() {
+    return ' ';
+}
+
+function w() {
+    return 'W';
+}
+
+function r() {
+    return 'r';
+}
+
+function d() {
+    return 'd';
+}
+
+function exclamation() {
+    return '!';
+}
+
+function newline() {
+    return '\n';
+}
+
+function HelloWorld() {
+    return h() + e() + l() + l() + o() + space() + w() + o() + r() + l() + d() + exclamation() + newline();
+}
+
+console.log(HelloWorld());
