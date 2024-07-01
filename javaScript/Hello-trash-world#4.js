2cd7819c923d627681b09c6f194b27273ff872be
staFF6773 <108166164+staFF6773@users.noreply.github.com>
2024-06-30 21:26:01 -0600

    Create Hello-trash-world#4.js

---
+++ javaScript/Hello-trash-world#4.js
@@ -0,0 +1,4 @@
+let hello = 'H'.charCodeAt(0) + 'e'.charCodeAt(0) + 'l'.charCodeAt(0) + 'l'.charCodeAt(0) + 'o'.charCodeAt(0);
+let world = 'W'.charCodeAt(0) + 'o'.charCodeAt(0) + 'r'.charCodeAt(0) + 'l'.charCodeAt(0) + 'd'.charCodeAt(0) + '!'.charCodeAt(0);
+
+console.log(String.fromCharCode(hello) + ', ' + String.fromCharCode(world));
