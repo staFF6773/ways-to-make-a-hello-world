4e6d559ab53ece6e560430f2152279a2e2dbe3d0
staFF6773 <108166164+staFF6773@users.noreply.github.com>
2024-06-30 21:19:43 -0600

    Create Hello-trash-world#1.go

---
+++ golang/Hello-trash-world#1.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+    var h, e, l1, l2, o, comma, space, w, r, d string
+    h = "H"
+    e = "e"
+    l1 = "l"
+    l2 = "l"
+    o = "o"
+    comma = ","
+    space = " "
+    w = "W"
+    r = "r"
+    d = "d"
+
+    fmt.Print(h)
+    fmt.Print(e)
+    fmt.Print(l1)
+    fmt.Print(l2)
+    fmt.Print(o)
+    fmt.Print(comma)
+    fmt.Print(space)
+    fmt.Print(w)
+    fmt.Print(o)
+    fmt.Print(r)
+    fmt.Print(l1)
+    fmt.Print(l2)
+    fmt.Println(d)
+}
