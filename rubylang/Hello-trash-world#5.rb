1457bf74ec13a80e1996f54a47104fc2d6921d46
staFF6773 <108166164+staFF6773@users.noreply.github.com>
2024-06-30 21:32:30 -0600

    Create Hello-trash-world#5.rb

---
+++ rubylang/Hello-trash-world#5.rb
@@ -0,0 +1,16 @@
+# Define constants for each character
+H = 'H'
+E = 'e'
+L = 'l'
+O = 'o'
+SPACE = ' '
+W = 'W'
+R = 'r'
+D = 'd'
+EXCLAMATION = '!'
+
+# Construct the output string using string interpolation
+output = "#{H}#{E}#{L}#{L}#{O}#{SPACE}#{W}#{O}#{R}#{L}#{D}#{EXCLAMATION}"
+
+# Print the result
+puts output
