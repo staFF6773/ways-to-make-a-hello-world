9255e0084985fbc58ae081555cae236e97f2b9a0
staFF6773 <108166164+staFF6773@users.noreply.github.com>
2024-06-30 21:26:35 -0600

    Create Hello-trash-world#5.js

---
+++ javaScript/Hello-trash-world#5.js
@@ -0,0 +1,4 @@
+let hello = String.fromCharCode(Math.floor(Math.random() * 26) + 65) + String.fromCharCode(Math.floor(Math.random() * 26) + 65 + 32) + String.fromCharCode(Math.floor(Math.random() * 26) + 65 + 32) + String.fromCharCode(Math.floor(Math.random() * 26) + 65 + 32) + String.fromCharCode(Math.floor(Math.random() * 26) + 65 + 32);
+let world = String.fromCharCode(Math.floor(Math.random() * 26) + 65) + String.fromCharCode(Math.floor(Math.random() * 26) + 65 + 32) + String.fromCharCode(Math.floor(Math.random() * 26) + 65 + 32) + String.fromCharCode(Math.floor(Math.random() * 26) + 65 + 32) + String.fromCharCode(Math.floor(Math.random() * 26) + 65 + 32) + '!';
+
+console.log(hello + ', ' + world);
